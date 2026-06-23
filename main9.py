21f5196a0a13ec06cd9c10466ef6468d1781ef5b
cheadaalin <cheadaalin01@gmail.com>
2026-06-23 22:16:52 +0700

    Update main9.py

--- main9.py
+++ main9.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     print("✅ Running session")
     driver = setup("SophSanto252")
     # smooth_scroll(driver)
-    work(driver,"SophSanto252","https://x.com/Viviwie6c")
+    work(driver,"SophSanto252","https://x.com/AngeTjand936")
     
 #https://x.com/SophSanto252
